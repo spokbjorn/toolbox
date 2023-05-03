1a699d83eb7dae87125befd29f723b51ad7ee49d
Andreas <andreas.everos@gmail.com>
2023-05-03 21:44:50 +0200

    Add Maven publish and signing

--- tuple/build.gradle.kts
+++ tuple/build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
     `java-library`
+    `maven-publish`
+    signing
 }
 
 group = "org.spokbjorn"
-version = "1.0.0"
+version = "0.0.1-SNAPSHOT"
 
 dependencies {
     compileOnly(libs.jetbrains.annotations)
@@ -15,4 +17,49 @@ dependencies {
 java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("tuple") {
+            groupId = "org.spokbjorn"
+            artifactId = "tuple"
+            version = "0.0.1-SNAPSHOT"
+
+            from(components["java"])
+            pom {
+                name.set("Tuple")
+                description.set("A collection of tuples")
+                url.set("http://github.com/spokbjorn/toolbox")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                    }
+                }
+                scm {
+                    connection.set("scm:git://github.com/spokbjorn/toolbox.git")
+                    developerConnection.set("scm:git://github.com/spokbjorn/toolbox.git")
+                    url.set("http://github.com/spokbjorn/toolbox")
+                }
+            }
+            repositories {
+                maven {
+                    credentials {
+                        username = project.properties["ossrhUsername"].toString()
+                        password = project.properties["ossrhPassword"].toString()
+                    }
+
+                    url = if (version.toString().endsWith("-SNAPSHOT")) {
+                        uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+                    } else {
+                        uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+                    }
+                }
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["tuple"])
 }
